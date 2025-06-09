7b82e13ce4f40d003ca175598c21d4a83f1e6003
srinivasandurairaj <mailtosrinivasandurairaj@gmail.com>
2025-06-09 23:56:04 +0100

    removed additional vars

--- src/app/api/profile/route.ts
+++ src/app/api/profile/route.ts
@@ -1,6 +1,3 @@
-export const dynamic = "force-dynamic"; // Ensures the route is dynamic
-export const revalidate = 60; // Revalidate every 60 seconds (adjust as needed)
-
 import { revalidatePath } from "next/cache";
 import { profileData } from "./data/profile";
 
