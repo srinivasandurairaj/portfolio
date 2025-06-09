cda783e443c8461cd172c4eef48c462f82e201ee
srinivasandurairaj <mailtosrinivasandurairaj@gmail.com>
2025-06-09 23:35:37 +0100

    Reverted the wrapper changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,50 @@
-import { Wrapper } from "./wrapper";
+"use client";
+
+import { useState, useEffect } from "react";
+
+import { AboutMe } from "@/components/about-me";
+import { Certifications } from "@/components/certifications/certifications";
+import { Form } from "@/components/form";
+import { Footer } from "@/components/footer";
+import { initialState } from "@/components/provider/initial-data";
+import { PersonCard } from "@/components/person-card";
+import { ProfileContext } from "@/components/provider/profile-provider";
+import { Stack } from "@/components/stack";
+import { WorkExperience } from "@/components/work-experience";
+
+import styles from "./page.module.css";
 
 const Home = () => {
-  return <Wrapper />;
+  const { container } = styles;
+  const [data, setData] = useState(initialState);
+
+  const fetchData = async () => {
+    const res = await fetch("/api/profile", {
+      next: {
+        revalidate: 1,
+      },
+    });
+    const data = await res.json();
+    setData(data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <ProfileContext.Provider value={data}>
+      <div className={container}>
+        <PersonCard />
+        <AboutMe />
+        <WorkExperience />
+        <Certifications />
+        <Stack />
+        <Form />
+        <Footer />
+      </div>
+    </ProfileContext.Provider>
+  );
 };
 
 export default Home;
