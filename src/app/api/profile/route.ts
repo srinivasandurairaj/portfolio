d6462d30748526b24e9f9ffc4cb023921d17e87e
srinivasandurairaj <mailtosrinivasandurairaj@gmail.com>
2025-06-09 23:59:00 +0100

    added all the config back

--- src/app/api/profile/route.ts
+++ src/app/api/profile/route.ts
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic"; // Ensures the route is dynamic
+export const revalidate = 60; // Revalidate every 60 seconds (adjust as needed)
+
 import { revalidatePath } from "next/cache";
 import { profileData } from "./data/profile";
 
