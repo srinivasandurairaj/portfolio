3a729d2ef9e34dc74a3ff3a4aa8c40d51c78f0de
srinivasandurairaj <mailtosrinivasandurairaj@gmail.com>
2025-06-09 23:54:29 +0100

    removed next config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  distDir: "dist",
-  images: { unoptimized: true },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
