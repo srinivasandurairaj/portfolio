3d34b3daff5b079e7a7f45ce4dba458033ae85f9
srinivasandurairaj <mailtosrinivasandurairaj@gmail.com>
2025-06-09 22:54:08 +0100

    Updated config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-
-  /* config options here */
+  reactStrictMode: true,
+  distDir: "dist",
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
--- src/app/api/profile/route.ts
+++ src/app/api/profile/route.ts
@@ -1,6 +1,9 @@
+import { revalidatePath } from "next/cache";
+
 import { profileData } from "./data/profile";
 
 export async function GET() {
+  revalidatePath("/profile");
   return new Response(JSON.stringify(profileData), {
     status: 200,
     headers: { "Content-Type": "application/json" },
