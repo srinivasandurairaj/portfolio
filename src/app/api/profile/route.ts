820dcc02f8476df6934d978e69a564c1df09f8e3
srinivasandurairaj <mailtosrinivasandurairaj@gmail.com>
2025-06-13 18:53:35 +0100

    Removed API integration

--- src/app/api/profile/route.ts
+++
@@ -1,8 +0,0 @@
-import { profileData } from "./data/profile";
-
-export async function GET() {
-  return new Response(JSON.stringify(profileData), {
-    status: 200,
-    headers: { "Content-Type": "application/json" },
-  });
-}
